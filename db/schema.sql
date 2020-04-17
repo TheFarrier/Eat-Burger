8164c249657810c62b6d93ed3eb4901f87f86293
The Farrier <nkfarrey@gmail.com>
2020-04-17 16:11:12 -0500

    initialized burger database schema

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers (
+  id int AUTO_INCREMENT PRIMARY KEY,
+  burger_name varchar(50) NOT NULL,
+  devoured boolean,
+);
