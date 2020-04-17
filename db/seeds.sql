2bf880e986bcf62a16323929592697653d2f0d1c
The Farrier <nkfarrey@gmail.com>
2020-04-17 16:15:38 -0500

    created seed file for burgers database

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("The Beast", true)
+INSERT INTO burgers (burger_name, devoured) VALUES ("The Beast Deluxe", false)
+INSERT INTO burgers (burger_name, devoured) VALUES ("Sliders", false)
