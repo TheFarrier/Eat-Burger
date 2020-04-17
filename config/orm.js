b721ce3253739eb10daa2664d1070310371e7990
The Farrier <nkfarrey@gmail.com>
2020-04-17 16:59:27 -0500

    started basic orm template

---
+++ config/orm.js
@@ -0,0 +1,15 @@
+const connection = require("./connection.js")
+
+var orm = {
+  selectAll: async function(){
+
+  },
+  insertOne: async function(){
+
+  },
+  updatedOne: async function(){
+
+  }
+}
+
+module.exports = orm;
