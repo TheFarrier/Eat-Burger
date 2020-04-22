a19b7a1616e1144811a8427120b426bc4bd3164c
The Farrier <nkfarrey@gmail.com>
2020-04-22 16:26:21 -0500

    corrected database name, removed personal password

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,15 @@
 const mysql = require("mysql")
 
 const connection = mysql.createConnection({
-  host:"localhost",
+  host: "localhost",
   port: 3306,
   user: "root",
-  password: "password",
-  database: "burger_db"
+  password: "",
+  database: "burgers_db"
 });
 
-connection.connect(function(err){
-  if(err) throw err;
+connection.connect(function (err) {
+  if (err) throw err;
   console.log(`connected as id + ${connection.threadId}`);
 });
 
