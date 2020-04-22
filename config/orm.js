a24a18d23b49be1ebc4f4140808664560117996c
The Farrier <nkfarrey@gmail.com>
2020-04-22 16:06:54 -0500

    created update orm function

--- config/orm.js
+++ config/orm.js
@@ -25,10 +25,16 @@ var orm = {
       }
     )
   },
-  updatedOne: async function () {
-    connection.query(`UPDATE burgers`,
+  updatedOne: async function (id, cb) {
+    connection.query(`UPDATE burgers SET devoured = true WHERE ?`,
+      {
+        id: id,
+      },
       function (err, result) {
-
+        if (err) {
+          throw err;
+        }
+        cb(result)
       }
     )
   }
