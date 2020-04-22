61d0ad3906d21436cc307d43e1313688bad67eb4
The Farrier <nkfarrey@gmail.com>
2020-04-22 16:07:48 -0500

    edited PUT as it only needs id passed to it

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -23,19 +23,15 @@ router.post("/api/burgers", function (req, res) {
 router.put("/api/burgers/:id", function (req, res) {
   var condition = "id = " + req.params.id;
 
-  console.log("devoured:", devoured);
+  console.log("devoured:", condition);
 
-  burger.updateOne(
-    {
-      devoured: true
-    },
-    devoured,
+  burger.updateOne(req.params.id,
     function (result) {
       if (result.changedRows === 0) {
         // If no rows were changed, then the ID must not exist, so 404
         return res.status(404).end();
       }
-      res.status(200).end();
+      res.redirect("/");
 
     }
   );
